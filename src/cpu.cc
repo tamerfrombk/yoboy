03b5e4d15b48a88b672022f32a1bec8509085371
Tamer Aly <taly.dev75@gmail.com>
2019-10-10 22:46:57 -0400

    Implement 0x00 as an example.

--- src/cpu.cc
+++ src/cpu.cc
@@ -21,14 +21,17 @@ yb::CPU::CPU(yb::MMU* mmu)
 uint8_t yb::CPU::cycle()
 {
     // fetch
-   std::printf("Fetching from 0x%.4X.\n", PC.value);
-   const uint8_t op = mmu_->read8(PC.value);
+   yb::log("Fetching from 0x%.4X.\n", PC.value);
+   uint8_t op = mmu_->read8(PC.value);
 
    // decode
-   yb::Instruction inst = yb::INSTRUCTIONS.at(op);
+   const yb::Instruction& inst = yb::INSTRUCTIONS.at(op);
 
    // execute
    switch (op) {
+    case 0x00:
+        PC.value += inst.length;
+        return inst.cycles;
     default:
         yb::exit("Unknown instruction 0x%.2X.\n", op);
         return 0;
